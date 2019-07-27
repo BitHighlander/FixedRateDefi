e793e1e9dc8d7ebe02b07d8f1c3ab536b05ba35c
highlander <bithighlander@gmail.com>
2019-07-27 17:32:57 -0600

    begin contract work

--- src/store.js
+++ src/store.js
@@ -5,10 +5,29 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    web3: {
+      isInjected: false,
+      web3Instance: null,
+      networkId: null,
+      coinbase: null,
+      balance: null,
+      error: null
+    },
+    contractInstance: null
   },
   mutations: {
-
+    registerWeb3Instance (state, payload) {
+      console.log('registerWeb3instance Mutation being executed', payload)
+      let result = payload
+      let web3Copy = state.web3
+      web3Copy.coinbase = result.coinbase
+      web3Copy.networkId = result.networkId
+      web3Copy.balance = parseInt(result.balance, 10)
+      web3Copy.isInjected = result.injectedWeb3
+      web3Copy.web3Instance = result.web3
+      state.web3 = web3Copy
+      //pollWeb3()
+    }
   },
   actions: {
 
